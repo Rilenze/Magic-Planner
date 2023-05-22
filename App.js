1e576199c32bbbb685a22e92078c9e37b702b2ae
Rijad Burović <rburovic1@etf.unsa.ba>
2023-05-22 20:41:39 +0200

    React Native app created with expo

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const API_BASE_URL = "https://zavrsni-back.herokuapp.com";
+
+  useEffect(() => {
+    async function fetchTasks() {
+      try {
+        const response = await fetch(`${API_BASE_URL}/api/v1/task`, {
+          mode: "cors",
+        });
+        const data = await response.json();
+        console.log(data);
+        setTasks(data);
+      } catch (error) {
+        console.error("Failed to fetch tasks:", error);
+      }
+    }
+
+    fetchTasks();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mujo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
